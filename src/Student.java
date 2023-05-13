d5dad26ca3180deae811f120e73f3ad0f504c500
leduyphong123 <leduyphongck789@gmail.com>
2023-05-13 22:39:58 +0700

    finnish:

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private String name="Join";
+    private String classes="CO2";
+    Student(){}
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setClasses(String classes){
+        this.classes=classes;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args){
+        Student sv=new Student();
+        sv.setClasses("C04");
+        sv.setName("Phong dev");
+    }
+}
